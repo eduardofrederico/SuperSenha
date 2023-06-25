5fda4907e1845e4cb5c5faacfdc659efb3ab4b53
Eduardo Frederico <eduardo.frederico@icloud.com>
2023-06-25 12:02:20 -0300

    Initial Commit

---
+++ SuperSenha/SuperSenhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSenhaApp.swift
+//  SuperSenha
+//
+//  Created by Eduardo Frederico on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperSenhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
